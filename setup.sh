57622a5d9e33a4de0ed2fca84b82e7464ae11672
fyx <7473283+fyxme@users.noreply.github.com>
2017-08-27 21:15:53 +1000

    Refactored code

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
+#! /usr/bin/env bash
+
 echo "Downloading required libraries"
 
 pip install Pillow
 pip install numpy scipy matplotlib ipython jupyter pandas sympy nose
 pip install imgurpython
 
-echo "Downloading daltonizer"
-
-if [[ -ne "daltonize"]]; then
-    git clone https://github.com/joergdietrich/daltonize.git
-fi
-
 echo "Setting up required folders"
 
-if [[ -ne "img" ]]; then
+if [[ ! -d "img" ]]; then
     mkdir img
 fi
+
+echo ">> Please update credentials.py with your credentials"
+echo ">> Then run get_imgur_token.py to get imgur access and refresh token"
