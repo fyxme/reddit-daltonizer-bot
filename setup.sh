33febf8d7631f59d06ce4000a4d436be872b0cac
fyx <7473283+fyxme@users.noreply.github.com>
2017-08-27 23:42:08 +1000

    Removed /img folder, daltonizer and other depracted files

--- setup.sh
+++ setup.sh
@@ -6,12 +6,6 @@ pip install Pillow
 pip install numpy scipy matplotlib ipython jupyter pandas sympy nose
 pip install imgurpython
 
-echo "Setting up required folders"
-
-if [[ ! -d "img" ]]; then
-    mkdir img
-fi
-
 echo ">> Update credentials-example.py with your credentials"
 echo ">> Rename credentials-example.py to credentials.py"
 echo ">> Then run get_imgur_token.py to get imgur access and refresh token"
